49e37048e8ae3d8f43069eac51c569fbf8429656
ajlohr270 <andrew.lohr@gmail.com>
2013-04-26 13:24:38 -0400

    first

---
+++ 631.v
@@ -0,0 +1,46 @@
+
+Require Export Sflib.
+
+Inductive variable : Type := 
+|var : id -> variable.
+
+Inductive orambank : Type :=
+|bank : nat -> orambank.
+
+
+Inductive expression : Type :=
+|expr : variable -> expression
+|exop : expression -> expression -> expression
+|exarr: variable -> expression -> expression
+|exnum: nat -> expression.
+
+Inductive location : Type :=
+|addr : nat -> location
+|oram : orambank -> location.
+
+Inductive statement : Type :=
+|skip : statement
+|assign: variable -> expression -> statement
+|arrasign: variable -> expression -> expression -> statement
+|stif: expression -> program -> program -> statement
+|stwhile: expression -> program -> statement
+
+with program : Type := 
+|line : location -> statement -> program
+|progcat : program -> program -> program.
+
+Inductive mtonat : Type :=
+| natl.
+
+Inductive label :  Type :=
+|low: label
+|o_high : orambank -> label.
+
+Inductive mtoarray : Type :=
+|arrl.
+
+Inductive labeledType : Type :=
+|larr : mtoarray -> label -> labeledType
+|lnat : mtonat -> label -> labeledType.
+
+Inductive  
