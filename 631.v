fef244e25a27c4cb400424c58fbaf401651f4554
ajlohr270 <andrew.lohr@gmail.com>
2013-04-26 17:52:29 -0400

    alot of typing

--- 631.v
+++ 631.v
@@ -11,7 +11,7 @@ Inductive orambank : Type :=
 
 
 Inductive expression : Type :=
-|expr : variable -> expression
+|exvar : variable -> expression
 |exop : expression -> expression -> expression
 |exarr: variable -> expression -> expression
 |exnum: nat -> expression.
@@ -23,7 +23,7 @@ Inductive location : Type :=
 Inductive statement : Type :=
 |skip : statement
 |assign: variable -> expression -> statement
-|arrasign: variable -> expression -> expression -> statement
+|arrassign: variable -> expression -> expression -> statement
 |stif: expression -> program -> program -> statement
 |stwhile: expression -> program -> statement
 
@@ -31,19 +31,15 @@ with program : Type :=
 |line : location -> statement -> program
 |progcat : program -> program -> program.
 
-Inductive mtonat : Type :=
-| natl.
 
 Inductive label :  Type :=
 |low: label
 |o_high : orambank -> label.
 
-Inductive mtoarray : Type :=
-|arrl.
 
 Inductive labeledType : Type :=
-|larr : mtoarray -> label -> labeledType
-|lnat : mtonat -> label -> labeledType.
+|larr :  label -> labeledType
+|lnat :  label -> labeledType.
 
 Definition mtojoin l1 l2 : label := 
 match l1 with
@@ -51,15 +47,11 @@ match l1 with
 |o_high a => l1
 end.
 
-Definition lable l1 l2 : bool :=
-match l1 with 
-|low => true
-|o_high _ =>
-    (match l2 with
-    |low => false
-    |o_high _ => true 
-    end)
-end.
+
+(**** NOTICE, excludes requirement that second not be a program location ****)
+Inductive lable : label -> label -> Prop :=
+|fst_low : forall l, lable low l
+|snd_high: forall l n, lable l (o_high n).
 
 
 Definition environment := variable -> (option labeledType).
@@ -90,8 +82,46 @@ Inductive tracePequiv: TracePat -> TracePat -> Prop:=
 (tracePequiv (concat T11 T21) (concat T12 T22))
 .
 
+Definition evt l t:  TracePat :=
+match l with
+|low => t
+|o_high a => Orambank a
+end.
+
 Inductive exprTyping: environment -> expression ->labeledType -> TracePat ->Prop :=
-|T-Var : forall
+|TVar : forall (gamma:environment) (x:variable) l, 
+((gamma x) =(Some (lnat l)))  -> (exprTyping gamma (exvar x) (lnat l) (evt l (Read x)))
+|TCon : forall (gamma:environment) n, exprTyping gamma (exnum n) (lnat low) epsilon
+|TOp : forall (gamma:environment) e1 e2 l1 l2 T1 T2, 
+(exprTyping gamma e1 (lnat l1) T1) -> (exprTyping gamma e2 (lnat l2) T2) ->
+(exprTyping gamma (exop e1 e2) (lnat (mtojoin l1 l2)) (concat T1 T2))
+|TArr : forall (gamma:environment) x e l l2 T, ((gamma x) =(Some (larr l))) -> 
+(exprTyping gamma e (lnat l2) T) -> (lable l2 l) ->
+(exprTyping gamma (exarr x e) (lnat (mtojoin l l2)) (concat T (evt l (Readarr x)))).
+
+Definition select t1 t2:TracePat :=
+
+
+Inductive statementTyping: environment -> label -> statement -> TracePat -> Prop :=
+|TSkip : forall gamma l, statementTyping gamma l skip epsilon
+|TAsn : forall gamma e l T x l0 l1, (exprTyping gamma e (lnat l) T) -> 
+(gamma x = Some (lnat l1)) -> (lable (mtojoin l0 l) l1) ->
+(statementTyping gamma l0 (assign x e) (concat T (evt l1 (Write x))))
+|TAAsn : forall gamma e1 e2 l1 l2 T1 T2 l0 l x,
+(exprTyping gamma e1 (lnat l1) T1) -> (exprTyping gamma e2 (lnat l2) T2) ->
+((gamma x) = Some (larr l)) -> (lable (mtojoin l1 (mtojoin l2 l0)) l) ->
+(statementTyping gamma l0 (arrassign x e1 e2) (concat T1 (concat T2 (evt l (Writearr x)))))
+|TCond : forall gamma e l l0 T1 T2 T S1 S2,
+(exprTyping gamma e (lnat l) T) ->
+(statementTyping gamma (mtojoin l l0) S1 T1) ->
+(statementTyping gamma (mtojoin l l0) S2 T2) ->
+(((mtojoin l l0) <> low) -> (TracePequiv T1 T2)) ->
+(statementTyping gamma l0 (stif e S1 S2) (concat T (select(T1,T2))))
+|TWhile : forall gamma e l l0 T1 T2 S p,
+(exprTyping gamma e (lnat l) T1) ->
+(statementTyping gamma l0 S T2) ->
+(lable (mtojoin l l0) low) ->
+(statementTyping gamma l0 (line p (exwhile e S )) (Loop p T1 T2)).
 
 
 Definition memory := variable -> (option ).
