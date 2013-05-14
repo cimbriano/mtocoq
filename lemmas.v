a566b81fc1124f9a9f51c7cbd50fb2f475c43817
cimbriano <Christopher.Imbriano@gmail.com>
2013-05-13 20:51:35 -0400

    Added formatting

--- lemmas.v
+++ lemmas.v
@@ -18,11 +18,11 @@ forall M1 M2 t1 M1' t2 M2', (lowEquivalentMem M1 M2) ->
 ((traceequiv t1 t2) /\ (lowEquivalentMem M1' M2')).
 
 Fixpoint tracelen (t:trace) : nat :=
-match t with
-|epsilon => 0
-|concat t1 t2 => plus (tracelen t1) (tracelen t2)
-| _ => 1
-end.
+  match t with
+  | epsilon => 0
+  | concat t1 t2 => plus (tracelen t1) (tracelen t2)
+  | _ => 1
+  end.
 
 Lemma lemmaone : forall t1 t2, (traceequiv t1 t2) -> ((tracelen t1) = (tracelen t2)).
 Proof.
